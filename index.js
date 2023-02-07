131fdcb48e297038ac3a042ead43572cde4718ba
ikoote1 <ikootepreim@gmail.com>
2023-02-07 16:20:52 +0300

    Styling for Desktop version

---
+++ index.js
@@ -0,0 +1,14 @@
+const ham = document.querySelector('.menu-ba');
+const x = document.querySelector('.menu-2');
+const navMenu = document.querySelector('.menu-line');
+
+ham.addEventListener('click', () => {
+  navMenu.classList.toggle('active');
+});
+x.addEventListener('click', () => {
+  navMenu.classList.toggle('active');
+});
+document.querySelectorAll('.menu-br').forEach((n) => n.addEventListener('click', () => {
+  navMenu.classList.toggle('active');
+}));
+
