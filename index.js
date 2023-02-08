ba2ddc1a31c7db4a8aca28bc982217d6317ef3ef
ikoote1 <ikootepreim@gmail.com>
2023-02-08 13:55:32 +0300

    Started to work on dynamic part

--- index.js
+++ index.js
@@ -2,11 +2,8 @@ const ham = document.querySelector('.menu-ba');
 const x = document.querySelector('.menu-2');
 const navMenu = document.querySelector('.menu-line');
 
-ham.addEventListener('click', () => {
-  // navMenu.classList.toggle('active');
-  if (navMenu.style.display === 'none') {
-    navMenu.style.display = 'block'
-  }
+ham.addEventListener('click', function() {
+  navMenu.classList.toggle('active');
 });
 x.addEventListener('click', () => {
   navMenu.classList.toggle('active');
@@ -14,7 +11,5 @@ x.addEventListener('click', () => {
 document.querySelectorAll('.menu-br').forEach((n) => n.addEventListener('click', () => {
   navMenu.classList.toggle('active');
 }));
-let m=7;
-let y=5;
-console.log(m+y);
+
 
